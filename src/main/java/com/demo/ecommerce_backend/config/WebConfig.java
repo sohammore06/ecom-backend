dcff3b351d10f2a1f9c60c2f60eed09bc8667f96
mihir <mihirvargante11@gmail.com>
2025-07-27 13:17:31 +0530

    revert

--- src/main/java/com/demo/ecommerce_backend/config/WebConfig.java
+++ src/main/java/com/demo/ecommerce_backend/config/WebConfig.java
@@ -1,15 +1,15 @@
-// package com.demo.ecommerce_backend.config;
+package com.demo.ecommerce_backend.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
-//     @Override
-//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//         // Replace with the actual path to your uploads folder
-//         registry.addResourceHandler("/uploads/**")
-//                 .addResourceLocations("file:uploads/");
-//     }
-// }
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Replace with the actual path to your uploads folder
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+}
